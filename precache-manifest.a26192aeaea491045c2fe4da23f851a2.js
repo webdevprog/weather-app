208e5398300b861c5f3ec38e9c9e718ee1061eb2
Vitatiy <targetdeveloper57@gmail.com>
2020-07-27 16:03:51 +0300

    Updates

---
+++ precache-manifest.a26192aeaea491045c2fe4da23f851a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f2c000dd9c41ec922ceb716832ddf2",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "970f5b7cd640d8130206",
+    "url": "/weather-app/static/css/2.b4e72bf1.chunk.css"
+  },
+  {
+    "revision": "1479c6da27ee883318c2",
+    "url": "/weather-app/static/css/main.4288880a.chunk.css"
+  },
+  {
+    "revision": "970f5b7cd640d8130206",
+    "url": "/weather-app/static/js/2.41fc6550.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weather-app/static/js/2.41fc6550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1479c6da27ee883318c2",
+    "url": "/weather-app/static/js/main.1c3b92bd.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
